254325ddc18c5b83118518c04abbe8d04c235a58
YanisGar24 <janice.a.garciasamperio24@gmail.com>
2024-08-14 15:39:43 -0600

    Falta imprimir imagen

--- my-react-app/src/Components/About.jsx
+++ my-react-app/src/Components/About.jsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
 import { useState, useEffect } from 'react';
@@ -26,6 +25,8 @@ const About = () => (
             <h1>Hola, soy Janice Garcia</h1>
             <h2>Acabé la universidad pero aun no estoy titulada</h2>
             <p>Tengo 24 años. No se que hacer con mi vida, busco empleo para poder subsistir y hacer valer el titulo :'v </p>
+            <p>Correo de contacto: janice.a.garciasamperio24@gmail.com</p>
+            <p></p>
             <p><small>Hasta aqui mi reporte joaquin</small></p>
             </CardContent>
             
--- my-react-app/src/Components/Apod.jsx
+++ my-react-app/src/Components/Apod.jsx
@@ -1,9 +1,56 @@
 import { useState, useEffect } from 'react';
-
+import Progress from './Progress.jsx';
 import './APOD.css'
 
+const DATA_URL = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+
+{/* Funcion donde desplegar la info de la imagen */}
+const Apod = ({date, hdurl, explanation}) => {
+    return (
+        <div className='Apod--container'>
+            <h2 className='Apod--date'>{date}</h2>
+            <img src={hdurl} />
+            <p className='Apod--explanation'>{explanation}</p>
+        </div>
+    )
+}
 
 
+const Apods = () => {
+    const [apods, setApods] = useState([])
+
+    useEffect(() => {
+        fetchApods();
+    }, []);
+
+    const fetchApods = () => {
+        fetch(DATA_URL)
+            .then(res => res.json())
+            .then(data => setApods(data.hdurl))
+            .catch(err => console.error(err))
+    }
+
+    console.log(apods);
+    {/*     return (
+        <>
+            <h2>APOD</h2>
+            {
+                apods.length !== 0 
+                    ? 
+                    apods.map((apod, index) => (
+                        <Apod key={index} {...apod} />
+                    ))
+                    :
+                    <Progress/>
+            }
+            
+        </>
+    )
+*/}
+
+}
+
+{/* 
 const Apod = () => (
     <>
         <h1>Estamos en APOD</h1>
@@ -11,5 +58,6 @@ const Apod = () => (
         <p><small>Help</small></p>
     </>
 )
+*/}
 
-export default Apod;
+export default Apods;
